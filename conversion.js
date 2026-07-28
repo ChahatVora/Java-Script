c434bbd4a83e2b4fe85d447c8a3168dead9f33eb
Chahat Vora <chahatvora1008@gmail.com>
2026-07-28 22:29:23 +0530

    first day

---
+++ conversion.js
@@ -0,0 +1,20 @@
+let a = true
+let b = Number(a)
+// console.log(typeof b);
+// console.log(b);
+
+let c = 2
+let d = Boolean(c)
+// console.log(typeof d);
+// console.log(d);
+
+let e = null
+let f = String(e)
+console.log(typeof f);
+console.log(f);
+
+
+
+
+
+
---
+++ variables.js
@@ -0,0 +1,11 @@
+const studentId = 67
+let studentName = "Chahat Vora"
+var studentAge = 20
+studentEmail = "chahat69gmail.com"
+let studentmarks
+
+// studentId = 62
+console.log(studentId)
+console.table([studentId,studentName,studentAge,studentEmail,studentmarks])
+
+// Notes:- do not use var because of scope issue
