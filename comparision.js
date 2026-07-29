7df298fcaba34d4c905dc12e6d8aee080e715a18
Chahat Vora <chahatvora1008@gmail.com>
2026-07-29 07:47:54 +0530

    2

---
+++ comparision.js
@@ -0,0 +1,26 @@
+// console.log("1" > 1);
+// console.log("1" >= 1);
+
+// console.log(2 > 3);
+// console.log(2 < 3);
+// console.log(2 >= 3);
+// console.log(2 <= 3);
+// console.log(2 == 3);
+// console.log(2 === 3);
+
+// console.log(null > 0);
+// console.log(null < 0);
+// console.log(null >undefined;
+
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined >= 0);
+
+// Notes  "===" operator is use for checking value with data type 
+console.log("2"===2);
+
+
+
+
+
+
---
+++ operations.js
@@ -0,0 +1,41 @@
+let a = 3
+let b = -a
+// console.log(b);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%6);
+
+let c = "chahat"
+let d = "het"
+let e = c + " " + d
+// console.log(e);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+let f = 10
+g = f++
+console.log(g);
+console.log(f);
+
+let h = 10
+i = ++h
+console.log(i);
+console.log(h);
+
+
+
+
+
+
+
+
+
+
+
